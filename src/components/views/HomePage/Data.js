c3d889996ca9171fbef706bb9a4fc48559a624d2
Bogdan <bogdan_kolesov@bk.ru>
2021-08-23 22:16:12 +0600

    add: sections

--- src/components/views/HomePage/Data.js
+++ src/components/views/HomePage/Data.js
@@ -49,3 +49,19 @@ export const homeObjThree = {
     start: ''
 }
 
+export const homeObjFour = {
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    lightText: true,
+    buttonLabel: 'Get Started!',
+    description: 'We help buisne owners increase their revenue. Our team of unique specialust can help you acheive your buisness goals.',
+    headline: 'Lead Generation Specialist for Online Buisnesses',
+    topLine: 'Marceting Agency',
+    //TODO? img link
+    // img: require('../../../assets/images/HomePage/objOne/objOneImg.svg'),
+    alt: 'Image',
+    start: ''
+}
--- src/components/views/HomePage/HomePage.js
+++ src/components/views/HomePage/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import InfoSection from '../../organismes/InfoSections';
-import { homeObjOne } from './Data';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
 
 const HomePage = () => {
     return (
         <>
             <InfoSection {...homeObjOne} />
+            <InfoSection {...homeObjTwo} />
+            <InfoSection {...homeObjThree} />
+            <InfoSection {...homeObjFour} />
         </>
     );
 };
