d9ab0c81529d61a246cc13b85071e1502cf640a4
Bogdan <bogdan_kolesov@bk.ru>
2021-08-24 13:29:42 +0600

    add: FooterLinks

--- src/components/organismes/Footer/Footer.js
+++ src/components/organismes/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Container, Button, Input } from '../../atoms/';
+import { Button, Input } from '../../atoms/';
 import { Form } from '../../molecules';
+import { Link } from 'react-router-dom';
 
-const FooterContainer = styled(Container)`
+const FooterContainer = styled.div`
     background-color: #101522;
     padding: 4rem 0 2rem 0;
     display: flex;
@@ -33,6 +34,55 @@ const FooterSubText = styled.p`
     font-size: 20px;
 `;
 
+const FooterLinksContainer = styled.div`
+    width: 100%;
+    max-width: 1000px;
+    display: flex;
+    justify-content: center;
+
+    @media screen and (max-width: 820px){
+        padding-top: 32px;
+    }
+`;
+
+const FooterLinksWrapper = styled.div`
+    display: flex;
+
+    @media screen and (max-width: 820px){
+        align-items: center;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+`;
+
+const FooterLinkItems = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 16px;
+    text-align: left;
+    box-sizing: border-box;
+    @media screen and (max-width: 420px){
+    margin: 0;
+    padding: 10px;
+    width: 100%;
+    }
+`;
+
+const FooterLinkTitle = styled.h3`
+    margin-bottom: 16px;
+    color: #fff;
+`;
+
+const FooterLink = styled(Link)`
+    color: #fff;
+    text-decoration: none;
+    margin-bottom: 0.5rem;
+    &:hover{
+        color: #0467fb;
+        transition: 0.3s ease-out;
+    }
+`;
 
 const Footer = () => {
     return (
@@ -49,6 +99,50 @@ const Footer = () => {
                     <Button fontBig>Subscribe!</Button>
                 </Form>
             </FotterSubscription>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>
+                            About us:
+                        </FooterLinkTitle>
+                        <FooterLink to='/sign-up'>How it works</FooterLink>
+                        <FooterLink to='/testimonial'>Testimonials</FooterLink>
+                        <FooterLink to='/careers'>Careers</FooterLink>
+                        <FooterLink to='/investors'>Investors</FooterLink>
+                        <FooterLink to='/terms'>Terms of Service</FooterLink>
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>
+                            Contact us:
+                        </FooterLinkTitle>
+                        <FooterLink to='/sign-up'>How it works</FooterLink>
+                        <FooterLink to='/testimonial'>Testimonials</FooterLink>
+                        <FooterLink to='/careers'>Careers</FooterLink>
+                        <FooterLink to='/investors'>Investors</FooterLink>
+                        <FooterLink to='/terms'>Terms of Service</FooterLink>
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>
+                            Videos:
+                        </FooterLinkTitle>
+                        <FooterLink to='/sign-up'>How it works</FooterLink>
+                        <FooterLink to='/testimonial'>Testimonials</FooterLink>
+                        <FooterLink to='/careers'>Careers</FooterLink>
+                        <FooterLink to='/investors'>Investors</FooterLink>
+                        <FooterLink to='/terms'>Terms of Service</FooterLink>
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>
+                            Social media:
+                        </FooterLinkTitle>
+                        <FooterLink to='/sign-up'>How it works</FooterLink>
+                        <FooterLink to='/testimonial'>Testimonials</FooterLink>
+                        <FooterLink to='/careers'>Careers</FooterLink>
+                        <FooterLink to='/investors'>Investors</FooterLink>
+                        <FooterLink to='/terms'>Terms of Service</FooterLink>
+                    </FooterLinkItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
         </FooterContainer>
     );
 };
